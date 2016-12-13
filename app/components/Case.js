dd20627b0656efa46dc8a2c3dd88383881e688f6
Alice Chen <alice@perka.com>
2016-12-13 08:13:35 -0500

    Open known case screenshot in lightbox, still needs styles

--- app/components/Case.js
+++ app/components/Case.js
@@ -2,16 +2,31 @@ import React from 'react';
 
 import Col from 'muicss/lib/react/col';
 import styles from '../styles.css';
+import Lightbox from 'react-image-lightbox';
 
 export default class Case extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
   render() {
+    const {isOpen} = this.state;
+
     return (
       <Col md="8" md-offset="2">
-        <h2 className={styles.staffCardHeading}>{this.props.situation}</h2>
+        <h2>{this.props.situation}</h2>
         <div className={styles.bodyText}>
-          <img src={this.props.imgUrl} />
           <a className={styles.linkDecorated} href={this.props.url} target="_blank" >{this.props.url}</a>
         </div>
+        {this.props.imgUrl && <button onClick={() => this.setState({isOpen: true})}>See Screenshot</button>}
+        {isOpen && <Lightbox
+          mainSrc={this.props.imgUrl}
+          onCloseRequest={() => this.setState({ isOpen: false })}
+        />}
       </Col>
     );
   }
